c9f16d9ce80f84d6a3091a98fec6b47a3799e748
Aref <rf.mohammadzade@gmail.com>
2020-04-10 20:40:31 +0430

    add rout

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+include_once "./lib/Route.php";
+use app\lib\Rout;
+
+$rout = new Rout();
+$r= isset($_GET['r']) ? $_GET['r']:'home';
+$rout->run($r);
+
---
+++ lib/Route.php
@@ -0,0 +1,17 @@
+<?php
+namespace app\lib;
+
+define('ROOT', dirname(__DIR__));
+
+class Rout{
+    public function run($r){
+        $controller = ucfirst($r)."Controller";
+        if(file_exists(ROOT.'/controllers/'.$controller)){
+            echo "Ok";
+        }
+        else{
+            echo "No";
+        }
+        
+    }
+}
