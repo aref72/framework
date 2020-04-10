3332375dedd5f6fdcfea6d0bfdd6046812d71428
Aref <rf.mohammadzade@gmail.com>
2020-04-10 20:41:49 +0430

    add rout

--- lib/Route.php
+++ lib/Route.php
@@ -5,7 +5,7 @@ define('ROOT', dirname(__DIR__));
 
 class Rout{
     public function run($r){
-        $controller = ucfirst($r)."Controller";
+        $controller = ucfirst($r)."Controller.php";
         if(file_exists(ROOT.'/controllers/'.$controller)){
             echo "Ok";
         }
